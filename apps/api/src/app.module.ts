14351a3dfd59b05646dc23929c570c6818edc2cc
Jaderson Luca <jadersonlucas01@gmail.com>
2026-08-15 22:10:05 +0000

    correcoes comunicacao api

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
@@ -6,7 +7,7 @@ import { QueuesModule } from './queues/queues.module';
 import { RedisModule } from './redis/redis.module';
 
 @Module({
-  imports: [DatabaseModule, RedisModule, QueuesModule, AuthModule],
+  imports: [DatabaseModule, RedisModule, QueuesModule, AuthModule, JwtModule.register({})],
   controllers: [AppController],
 })
 export class AppModule {}
