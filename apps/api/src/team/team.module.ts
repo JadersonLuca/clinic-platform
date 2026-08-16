603f37f038647caef3286cff4505a9a56b9f6dd2
Jaderson Luca <jadersonlucas01@gmail.com>
2026-08-16 04:43:08 +0000

    Update TeamModule imports to include AuthModule

--- apps/api/src/team/team.module.ts
+++ apps/api/src/team/team.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
-import { DatabaseModule } from '../database/database.module';
+import { AuthModule } from '../auth/auth.module';
 import { PasswordService } from '../auth/password.service';
+import { DatabaseModule } from '../database/database.module';
 import { TeamController } from './team.controller';
 import { TeamService } from './team.service';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [AuthModule, DatabaseModule],
   controllers: [TeamController],
   providers: [TeamService, PasswordService],
 })
